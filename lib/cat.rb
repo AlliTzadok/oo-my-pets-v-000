d635e3ad18a5cdbe15fe3aa90289acd300b9fe32
Alli Tzadok <allisontzadok@gmail.com>
2018-01-23 20:09:34 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -6,7 +6,7 @@ class Cat
   #name cannot be changed. - use a attr_reader
   #initialize with a nervous mood
   #mood can be changed - attr_accessor
-  def initialize(name, mood)
+  def initialize(name)
     @name = name
     @mood = nervous
   end
