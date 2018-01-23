ab4abda3bda26aff19b8a988cdd6ff3ee912e8f9
Alli Tzadok <allisontzadok@gmail.com>
2018-01-23 20:07:30 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,4 +1,14 @@
 class Fish
+  attr_accessor :mood
+  attr_reader :name
 
+  #can initialize a Cat with a name
+  #name cannot be changed. - use a attr_reader
+  #initialize with a nervous mood
+  #mood can be changed - attr_accessor
+  def initialize(name, mood)
+    @name = name
+    @mood = nervous
+  end
 
 end
