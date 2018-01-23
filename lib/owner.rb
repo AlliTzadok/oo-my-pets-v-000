b892c2ae5562cd215ba4addbcc27332ec9fa5265
Alli Tzadok <allisontzadok@gmail.com>
2018-01-23 20:26:12 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -20,10 +20,15 @@ class Owner
     @@owners.clear
   end
 
+  #initialize with a species, and species can't be changed.
   def initialize(species)
     @species = species
 
     @pets = {:fishes => [], :cats => [], :dogs=> []}
+  end
+
+  def say_species
+    "I am a #{species}."
   end
 
 
