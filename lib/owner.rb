dece716ae09407f2c674f3ad9f57dc696c457d43
Alli Tzadok <allisontzadok@gmail.com>
2018-01-23 20:18:05 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -10,8 +10,11 @@ class Owner
     @@owners
   end
 
-  #class method - reset_all can count how many owners have been created
-  #class method - it can reset the owners that have been created - clear...
+  #class method -  can count how many owners have been created
+  def self.count
+    @@owners.size
+
+  #class method - reset_all -> reset the owners that have been created - clear...
   def self.reset_all
     @@owners.clear
   end
